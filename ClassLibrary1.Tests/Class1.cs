6611a4af5e4fe5557c970fff5fa11aef3359a8b9
kkolesnikov <kkolesnikov@hazeltree.com>
2019-04-17 13:06:45 +0300

    first commit

---
+++ ClassLibrary1.Tests/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClassLibrary3;
+using NUnit.Framework;
+
+namespace ClassLibrary1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        private Node ExpressionBuild(string expression)
+        {
+            var parts = expression.Split(">");
+            return new Node()
+            {
+                Id = parts[0],
+                Dependencies = parts.Length == 2 && !string.IsNullOrEmpty(parts[1])?parts[1].Split(",").ToList(): new List<string>()
+            };
+        }
+
+        private List<Node> ExpressionsBuild(params string[] expressions)
+        {
+            return expressions.Select(ExpressionBuild).ToList();
+        }
+
+        [Test]
+        public void SimpleCase()
+        {
+            var nodes = ExpressionsBuild(
+                "A>B,C",
+                "B>C,D",
+                "D>",
+                "C>"
+            );
+            var result = NodeParser.Parse(nodes);
+            Assert.AreEqual("D,C,B,A", result.Select(a=>a.Id).Aggregate((a,b)=>a+","+b));
+        }
+
+        [Test]
+        public void CycleDependency()
+        {
+            var nodes = ExpressionsBuild(
+                "A>B,C",
+                "B>C,D",
+                "D>",
+                "C>A"
+            );
+            try
+            {
+                var result = NodeParser.Parse(nodes);
+            }
+            catch (CycleDependencyException e)
+            {
+                CollectionAssert.AreEquivalent(new List<string>(){"A","B","C"}, e.UnresolvedNodes );
+            }
+        }
+
+        [Test]
+        public void EmptyCase()
+        {
+            var result = NodeParser.Parse(new List<Node>());
+            CollectionAssert.IsEmpty(result);
+        }
+
+        [Test]
+        public void ExceedExpression()
+        {
+            var nodes = ExpressionsBuild(
+                "A>B,E",
+                "B>C,D",
+                "D>F",
+                "C>"
+            );
+            try
+            {
+                var result = NodeParser.Parse(nodes);
+            }
+            catch (ExceedDependencyException e)
+            {
+                CollectionAssert.AreEquivalent(new List<string>() { "E","F" }, e.ExceedNodes);
+            }
+        }
+    }
+}
---
+++ ClassLibrary3/Node.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary3
+{
+    public class Node
+    {
+        public string Id { get; set; }
+        public List<string> Dependencies { get; set; } = new List<string>();
+    }
+
+    public static class NodeParser
+    {
+
+
+        private static void CheckExceed(List<Node> nodes)
+        {
+            var allNodesFromDepndencies = new HashSet<string>(nodes.SelectMany(a => a.Dependencies));
+            var allNodesFromRoot = nodes.Select(a => a.Id).ToList();
+            if (allNodesFromDepndencies.Except(allNodesFromRoot).Count()!=0)
+            {
+                throw new ExceedDependencyException(allNodesFromDepndencies.Where(a => !allNodesFromRoot.Contains(a))
+                    .ToList());
+            }
+        }
+
+        public static Queue<Node> Parse(List<Node> nodes)
+        {
+            CheckExceed(nodes);
+            HashSet<string> map = new HashSet<string>();
+            var source = nodes.ToList();
+            Queue<Node> result = new Queue<Node>();
+            int count = -1;
+            while (count!= map.Count)
+            {
+                count = map.Count;
+                var independent = ExtractAllIndependent(source, map);
+                foreach (var node in independent)
+                {
+                    result.Enqueue(node);
+                    map.Add(node.Id);
+                }
+
+                source.RemoveAll(a => map.Contains(a.Id));
+            }
+
+            if (result.Count != nodes.Count)
+            {
+                throw new CycleDependencyException(nodes.Select(a => a.Id).Except(result.Select(a => a.Id)).ToList());
+            }
+            return result;
+        }
+
+        private static List<Node> ExtractAllIndependent(List<Node> nodes, HashSet<string> map)
+        {
+            return nodes.Where(a => a.Dependencies.All(map.Contains)).ToList();
+        }
+    }
+
+    public class CycleDependencyException : Exception
+    {
+        public CycleDependencyException(List<string> unresolvedNodes)
+        {
+            UnresolvedNodes = unresolvedNodes;
+        }
+
+        public List<string> UnresolvedNodes { get; set; }
+
+    }
+
+    public class ExceedDependencyException : Exception
+    {
+        public List<string> ExceedNodes { get; set; }
+
+        public ExceedDependencyException(List<string> exceedNodes)
+        {
+            ExceedNodes = exceedNodes;
+        }
+    }
+}
